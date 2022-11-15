182285f98833fa00b030ecbf04bb58367dca8c09
vivignutz <vgnutzmann@gmail.com>
2022-11-15 13:22:00 +0000

    Add function to toggle cards and style that

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,21 @@
+//** anonymous function for an immediately start  */
 (function () {
     startGame();
 
     function startGame() {
-        for (var i = 0; i < 16; i++) {
-            var card = document.querySelector("#card" + i);
+        for (let i = 0; i < 16; i++) {
+            let card = document.querySelector("#card" + i);
             card.style.left = (i === 0 || i === 8) ? 5 + "px" : i % 8 * 165 + 5 + 5 + "px";
             card.style.top = i < 8 ? 5 + "px" : 250 + "px";
+
+            card.addEventListener("click", flipCard, false); //** evento: click, funcao disparada: flipcard, false (to be sure that it wont happen by mistake) */
+        }
+
+        //** function to toggle cards */
+        function flipCard() {
+            let faces = this.getElementsByClassName("face");
+            faces[0].classList.toggle("flipped");
+            faces[1].classList.toggle("flipped");
         }
     }
 }());
