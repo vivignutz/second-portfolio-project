b60e285923dd64632c3716c931a973a6425cdf08
vivignutz <vgnutzmann@gmail.com>
2022-11-17 08:17:59 +0000

    Changed some scripts at JS to try to flip cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,85 @@
-//** oldArray image wchich are listed in html */
+//** array to storage objects with src and id 1 to 6 */
 let images = [];
 
-for (let i = 1; i < 12; i++) {
+//** array to storage clicked cards +/
+let flippedCards = [];
+
+//** assigning the images to the cards  */
+for (let i = 0; i < 12; i++) {
     let img = {
-        src: "assets/images/" + i + ".png",
+        src: "images/" + i + ".png",
         id: i % 6
     };
+
+    //** push the images to array */
     images.push(img);
 }
 
 //** here starts the game */
-window.onload = startGame()
+startGame()
 
 function startGame() {
-    flippedCards = []; //deixar assim para que flipe
+    //** reseting the card */
+    flippedCards = [];
+
+    //** sorting cards */
     images = randomSort(images);
 
+    //** cards div w/classes front */
     let frontFaces = document.getElementsByClassName("front");
 
-    for (let i = 1; i < 12; i++) {
-        let card = document.getElementById("card" + i);
+    for (let i = 0; i < 12; i++) {
+        let card = document.querySelector("#card" + i);
+        //card.style.left = i % 6 === 0 ? 5 + "px" : i % 6 * 165 + 5 + "px";
+        //card.style.top = i < 6 ? 5 + "px" : 250 + "px";
 
-        card.addEventListener("click", flipCard);
+        card.addEventListener("click", flipCard, false);
 
-        frontFaces[i].style.background = "url('" + images[i - 1].src + "')";
-        frontFaces[i].setAttribute("id", images[i - 1].id);
+        //** aditioning images ids to the cards */
+        frontFaces[i].style.background = "url('" + images[i].src + "')";
+        frontFaces[i].setAttribute("id", images[i].id);
+    }
+}
+
+//** creating alleatory numbers for cards distribution */
+function randomSort(oldArray) {
+    //** empty newArray is created */
+    let newArray = [];
+
+    while (newArray.length !== oldArray.length) {
+        //** creates a variable i which gets a alleatory number */
+        let i = Math.floor(Math.random() * oldArray.length); //* alleatory value which gets a random value X elemtn numbers of oldArray
+        if (newArray.indexOf(oldArray[i]) < 0) {
+            newArray.push(oldArray[i]);
+        }
     }
 
-    //** creating alleatory numbers for cards distribution */
-    function randomSort(oldArray) {
+    //** retorning to the nweArray */
+    return newArray;
+}
 
-        //** newArray after creating new card sistribution */
-        let newArray = [];
+//** To flip up the cards */    
+function flipCard() {
+    if (flippedCards.length < 2) {
+        let faces = this.getElementsByClassName("face");
 
-        while (newArray.length !== oldArray.length) {
-            let i = Math.floor(Math.random() * oldArray.length); //* alleatory value which gets a random value X elemtn numbers of oldArray
-
-            if (newArray.indexOf(oldArray[i]) < 1) {
-                newArray.push(oldArray[i]);
-            }
+        //** to avoid to take card with same number */
+        if (faces[0].classList.length > 2) {
+            return;
         }
 
-        return newArray;
-    }
+        faces[0].classList.toggle("flipped");
+        faces[1].classList.toggle("flipped");
 
-    //** To flip up the cards */    
-    function flipCard() {
-        if (flippedCards.length < 2) {
-            let faces = this.getElementsByClassName("face"); //* the clicked card
+        //** How to flip only 2 cards and unflip both in the 3rd click */
+        //** in case of no match */
+        flippedCards.push(this);
+    } else {
+        flippedCards[0].childNodes[1].classList.toggle("flipped"); // childNodes to return newArray
+        flippedCards[0].childNodes[3].classList.toggle("flipped");
+        flippedCards[1].childNodes[1].classList.toggle("flipped");
+        flippedCards[1].childNodes[3].classList.toggle("flipped");
 
-            if (faces[0].classList.length > 2) {
-                return;
-            }
-
-            faces[0].classList.toggle("flipped"); //* 0 is back, 1 is
-            faces[1].classList.toggle("flipped");
-
-            //** How to flip only 2 cards and unflip in the 3rd click */
-            flippedCards.push(this);
-        } else {
-            flippedCards[0].childNodes[1].classList.toggle("flipped"); // childNodes to return newArray
-            flippedCards[0].childNodes[3].classList.toggle("flipped");
-            flippedCards[1].childNodes[1].classList.toggle("flipped");
-            flippedCards[1].childNodes[3].classList.toggle("flipped");
-
-            flippedCards = [];
-        }
+        flippedCards = [];
     }
 }
